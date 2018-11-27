0031461dc9b3dba961e89f3692d54a540aa09dd3
Fia <flaw.riette@gmail.com>
2018-11-27 20:18:10 +0700

    cobaaa

---
+++ avored.admin.webpack.mix.js
@@ -0,0 +1,19 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.config.fileLoaderDirs.fonts     = 'vendor/avored-admin/fonts';
+mix.config.fileLoaderDirs.images    = 'vendor/avored-admin/images';
+
+mix.js('packages/framework/resources/assets/js/app.js', 'public/vendor/avored-admin/js')
+    .sass('packages/framework/resources/assets/sass/app.scss', 'public/vendor/avored-admin/css')
+   // .copyDirectory('vendor/avored/framework/resources/assets/static/images','public/vendor/avored-admin/images');
